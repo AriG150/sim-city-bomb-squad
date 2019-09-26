cb4bc635d077b390178f023f10d74a32926e0bc2
Ari <gryphon150@gmail.com>
2019-09-26 14:35:27 -0700

    laid out html and css. some js.

--- bomb.js
+++ bomb.js
@@ -1,1 +1,26 @@
 console.log('loaded!');
+
+// Variables 
+const STARTING_TIME = 30;
+var remainingTime = 0; 
+var gameOver = false;
+var countDown = null;
+var delay = null;
+
+
+var wireState = {
+    blue: false,
+    green: false,
+    red: false,
+    white: false,
+    yellow: false
+}
+
+var wiresToCut = [];
+
+// DOM References
+var timer = document.getElementById("timer");
+var wireBox = document.getElementById("wirebox");
+var resetButton = document.querySelector('button');
+// TODO: add audio elements // 
+
