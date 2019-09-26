9fd5ab7e2363fc1d8ce535e7cc0abf5410991f70
Ari <gryphon150@gmail.com>
2019-09-26 15:43:43 -0700

    Started JS event handlers, variables, functions

--- bomb.js
+++ bomb.js
@@ -7,7 +7,7 @@ var gameOver = false;
 var countDown = null;
 var delay = null;
 
-
+//false = not cut, true = cut // 
 var wireState = {
     blue: false,
     green: false,
@@ -24,3 +24,61 @@ var wireBox = document.getElementById("wirebox");
 var resetButton = document.querySelector('button');
 // TODO: add audio elements // 
 
+// Event Listeners 
+wireBox.addEventListener('click', function(e) {
+    var color = e.target.alt
+    if (!wireState[color] && !gameOver){
+        // If wire isn't cute and game isn't over...
+        e.target.src = `img/cut-${color}-wire.png`;
+        //TODO: play cut audio//
+        wireState[color] = true;
+        //Check for correctness: is it i 'wiresToCut', if it is we need to splice it out by knowing its index
+        var wireIndex = wiresToCut.indexOf(color);
+        if(wireIndex > -1) {
+            //Correct wire cut 
+            console.log(`${color} at index ${wireIndex} was correct`);
+            wiresToCut.splice(wireIndex, 1);
+            //TODO: Check for a win here 
+        } else {
+            //Incorrect wire cut 
+            console.log(`${color} at index ${wireIndex} was wrong`);
+            //TODO: Start the death delay 
+        }
+
+    }
+});
+
+// Functions 
+function init(){
+    wiresToCut.length=0;
+    remainingTime = STARTING_TIME;
+    for(let wire in wireState) {
+        var rand = Math.random();
+        if (rand > 0){
+            wiresToCut.push(wire);
+        }
+    }
+    console.log(wiresToCut);
+    resetButton.disabled = true;
+    //TODO: Play the siren 
+    //TODO: Start the countdown 
+}
+
+function reset(){
+    //Marking all wires uncut 
+    for (let wire in wireState){
+        wireState[wire] = false;
+    }
+    var color;
+    for (let i = 0; i < wireBox.children.length; i++){
+        color = wireBox.children[i].alt
+        wireBox.children[i].src = `img/uncut-${color}-wire.png`
+    }
+    gameOver = false;
+    document.body.classList.toggle('exploded');
+    timer.classList.remove('green');
+    clearTimeout(delay);
+    clearTimeout(countdown);
+
+    //TODO: Stop playing audio (victory and/or explosion)
+}
